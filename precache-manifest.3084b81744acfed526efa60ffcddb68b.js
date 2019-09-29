f63ff868ad9d2c6188d9fab2a4bdfa636695f939
Radoslaw Matlak <radek1999@onet.com.pl>
2019-09-29 14:54:27 +0200

    Updates

---
+++ precache-manifest.3084b81744acfed526efa60ffcddb68b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8dd915371904db94cf3b8d72e304aaf",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "2d56d200b63fedb60429",
+    "url": "/portfolio/static/css/main.493c65cd.chunk.css"
+  },
+  {
+    "revision": "ebef1bd6ad2816354ccb",
+    "url": "/portfolio/static/js/2.38ef67db.chunk.js"
+  },
+  {
+    "revision": "2d56d200b63fedb60429",
+    "url": "/portfolio/static/js/main.ae17c60b.chunk.js"
+  },
+  {
+    "revision": "c7251a6339e2c36ec556",
+    "url": "/portfolio/static/js/runtime-main.611d5448.js"
+  },
+  {
+    "revision": "0f259bcfc7fed4c2ecb4eca1379ad094",
+    "url": "/portfolio/static/media/Radek.0f259bcf.png"
+  },
+  {
+    "revision": "742ae99e376f6b12daae9049a09527f0",
+    "url": "/portfolio/static/media/blur-codes-coding-577585.742ae99e.png"
+  },
+  {
+    "revision": "dc5cfa3aa99600199b74ddf0c88ae20e",
+    "url": "/portfolio/static/media/krecha1.dc5cfa3a.png"
+  },
+  {
+    "revision": "7c49d8c362dbb09285247ce009f03b60",
+    "url": "/portfolio/static/media/krecha2.7c49d8c3.png"
+  },
+  {
+    "revision": "ed35f2b4ad2cf7af91da686b9c8ead0f",
+    "url": "/portfolio/static/media/rm.ed35f2b4.png"
+  }
+]);
